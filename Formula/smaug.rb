4faa86a2b965e5604112a56735a1917484dddc30
Matt Pruitt <matt.pruitt@blockfi.com>
2021-02-28 19:55:52 +0700

    Fix linter

--- Formula/smaug.rb
+++ Formula/smaug.rb
@@ -1,7 +1,5 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
 class Smaug < Formula
-  desc "A tool to manage your DragonRuby Game Toolkit projects."
+  desc "Manages your DragonRuby Game Toolkit projects"
   homepage "https://smaug.dev"
   url "https://github.com/ereborstudios/smaug/archive/v0.1.0.tar.gz"
   sha256 "0fec3fd1718e683440b6021da8cecdfef8d4880d6a3ff2af6fb5131a4c991381"
