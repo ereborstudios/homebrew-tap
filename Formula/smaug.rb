709c6f11d8cf5d74970c6ba997b563774c16599a
Matt Pruitt <matt.pruitt@blockfi.com>
2021-03-12 19:06:20 +0700

    Update hash
    Closes #2.
    
    Signed-off-by: Matt Pruitt <matt@guitsaru.com>

--- Formula/smaug.rb
+++ Formula/smaug.rb
@@ -2,7 +2,7 @@ class Smaug < Formula
   desc "Manages your DragonRuby Game Toolkit projects"
   homepage "https://smaug.dev"
   url "https://github.com/ereborstudios/smaug/archive/v0.2.0.tar.gz"
-  sha256 "4ea97c8c1c9ebfec1a1dec51e81b53b91425cd74df864c5b6fa726aa59db44a3"
+  sha256 "41666db78bd22ec2eea245690eb6e7ce7f66c9bf67a8138b3e3b99e757d53656"
   license "AGPL-3.0-or-later"
 
   depends_on "rust" => :build
