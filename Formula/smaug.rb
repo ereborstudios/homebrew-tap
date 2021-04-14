82a0d409e82e20cf3e44a002f9ec0e5fb036f3bf
Matt Pruitt <matt.pruitt@blockfi.com>
2021-04-14 16:52:46 +0700

    Lowercase SHA

--- Formula/smaug.rb
+++ Formula/smaug.rb
@@ -2,7 +2,7 @@ class Smaug < Formula
   desc "Manages your DragonRuby Game Toolkit projects"
   homepage "https://smaug.dev"
   url "https://github.com/ereborstudios/smaug/archive/v0.2.3.tar.gz"
-  sha256 "b25D24147CB721A9F93DA9BA4ADFA30DB46A90970E28FA491044A1B46321001b"
+  sha256 "b25d24147cb721a9f93da9ba4adfa30db46a90970e28fa491044a1b46321001B"
   license "AGPL-3.0-or-later"
 
   depends_on "rust" => :build
