f298c0c339c8da825e9626429ba10db489f49379
Matt Pruitt <matt.pruitt@blockfi.com>
2021-04-14 17:04:21 +0700

    Lowercase SHA
    Closes #4.
    
    Signed-off-by: Matt Pruitt <matt@guitsaru.com>

--- Formula/smaug.rb
+++ Formula/smaug.rb
@@ -2,7 +2,7 @@ class Smaug < Formula
   desc "Manages your DragonRuby Game Toolkit projects"
   homepage "https://smaug.dev"
   url "https://github.com/ereborstudios/smaug/archive/v0.2.3.tar.gz"
-  sha256 "b25d24147cb721a9f93da9ba4adfa30db46a90970e28fa491044a1b46321001B"
+  sha256 "b25d24147cb721a9f93da9ba4adfa30db46a90970e28fa491044a1b46321001b"
   license "AGPL-3.0-or-later"
 
   depends_on "rust" => :build
