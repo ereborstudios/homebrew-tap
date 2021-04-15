c0b3dece9a1c9c1d1ec0e67fc70a1dc827200a5c
Matt Pruitt <matt.pruitt@blockfi.com>
2021-04-15 17:08:14 +0700

    v0.3.0
    Closes #5.
    
    Signed-off-by: Matt Pruitt <matt@guitsaru.com>

--- Formula/smaug.rb
+++ Formula/smaug.rb
@@ -1,14 +1,9 @@
 class Smaug < Formula
   desc "Manages your DragonRuby Game Toolkit projects"
   homepage "https://smaug.dev"
-  url "https://github.com/ereborstudios/smaug/archive/v0.2.3.tar.gz"
-  sha256 "b25d24147cb721a9f93da9ba4adfa30db46a90970e28fa491044a1b46321001b"
+  url "https://github.com/ereborstudios/smaug/archive/v0.3.0.tar.gz"
+  sha256 "512ab754d21d0a38ebbfc0efc325f98ef1c7f2b026fbe04d834b5e796205d318"
   license "AGPL-3.0-or-later"
-
-  bottle do
-    root_url "https://github.com/ereborstudios/homebrew-tap/releases/download/smaug-0.2.3"
-    sha256 cellar: :any, catalina: "bbb9795ad8caf849f50491abb20d8770c2c90c555727b3321b37eb880574e275"
-  end
 
   depends_on "rust" => :build
   depends_on "openssl@1.1"
