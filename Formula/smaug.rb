1d13d2a677e398eac011366457bd83dd96d07176
Matt Pruitt <matt@guitsaru.com>
2021-05-29 07:15:58 +0000

    smaug: add 0.3.1 bottle.

--- Formula/smaug.rb
+++ Formula/smaug.rb
@@ -5,6 +5,11 @@ class Smaug < Formula
   sha256 "01b321c9060d6104219d0ce770a2785b3aedd09a8dfcd69780d78bf41e19afad"
   license "AGPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/ereborstudios/homebrew-tap/releases/download/smaug-0.3.1"
+    sha256 cellar: :any, catalina: "7a8b8cdec13e862de3a309b86ad2a76d69a1b9dc3f81f4a29e5fcf98d8d8b5a4"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
