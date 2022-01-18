8bc7de227e6c26b5eb81d657fa2f2e81763a62bf
Matt Pruitt <matt@guitsaru.com>
2022-01-18 16:56:56 +0700

    release v0.4.0

--- Formula/smaug.rb
+++ Formula/smaug.rb
@@ -1,14 +1,9 @@
 class Smaug < Formula
   desc "Manages your DragonRuby Game Toolkit projects"
   homepage "https://smaug.dev"
-  url "https://github.com/ereborstudios/smaug/archive/v0.3.2.tar.gz"
-  sha256 "930422e0e210e7ecafd87a8baf57a1482ef891783f6da0877035dd6d93ba0b9e"
+  url "https://github.com/ereborstudios/smaug/archive/v0.4.0.tar.gz"
+  sha256 "79d86aa9b97188225682daefe78276fa14d4a6505e1b2e32a32e278269baabc4"
   license "AGPL-3.0-or-later"
-
-  bottle do
-    root_url "https://github.com/ereborstudios/homebrew-tap/releases/download/smaug-0.3.2"
-    sha256 cellar: :any, catalina: "652fc7efc299ded791e23b4905094a2c88563f9b40fe987a7d13c9ba836dd59b"
-  end
 
   depends_on "rust" => :build
   depends_on "openssl@1.1"
