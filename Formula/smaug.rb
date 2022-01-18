e0f1783689534ebc85e466a529cb35eb8e726239
Matt Pruitt <matt@guitsaru.com>
2022-01-18 17:21:12 +0700

    Update homepage

--- Formula/smaug.rb
+++ Formula/smaug.rb
@@ -1,6 +1,6 @@
 class Smaug < Formula
   desc "Manages your DragonRuby Game Toolkit projects"
-  homepage "https://smaug.dev"
+  homepage "https://github.com/ereborstudios/smaug"
   url "https://github.com/ereborstudios/smaug/archive/v0.4.0.tar.gz"
   sha256 "79d86aa9b97188225682daefe78276fa14d4a6505e1b2e32a32e278269baabc4"
   license "AGPL-3.0-or-later"
